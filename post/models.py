7d3ed3d34b71aef20b9e3a1b8d014c2b3d0e118c
Manuel Colorado <manuel1412@gmail.com>
2019-09-20 05:45:08 +0000

    Created post models

---
+++ post/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Post(models.Model):
+    """
+    Stores the information for a news or blog post for the site
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, 
+        default=timezone.now)
+    views = models.IntegerField(default=0)
+    tag=models.CharField(max_length = 30, blank=True, null=True)
+    image=models.ImageField(upload_to="img", blank=True, null=True)
+    author = models.ForeignKey(
+      get_user_model(),
+      on_delete=models.CASCADE
+    )
+    
+    
+    def __unicode__(self):
+        return self.title
