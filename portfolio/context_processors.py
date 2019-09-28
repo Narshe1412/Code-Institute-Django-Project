32cb34612278b16501fb44e2c331c4832c7eb604
Manuel Colorado <manuel1412@gmail.com>
2019-09-28 10:24:17 +0000

    added context processor to handle web constants

---
+++ portfolio/context_processors.py
@@ -0,0 +1,12 @@
+
+SITE_CONFIG = {
+    'COMPANY_NAME': "Save the World",
+    'COMPANY_PHONE': "555-332333",
+    'COMPANY_EMAIL': "save.the.world@fake.savetheworld.com",
+    'COMPANY_TWITTER': "@savetheworld.fake",
+    'COMPANY_FACEBOOK': "facebook.com"
+}
+
+def site_config(request):
+    return SITE_CONFIG
+    
